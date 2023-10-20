03cad366d57d8f8be4beaa4750d88941dc4e65cb
Mohamed AbouBakr <itsmoaboubakr@outlook.com>
2023-10-20 16:16:12 +0300

    Create HREC_config.h

---
+++ ECU1/03-HAL/HREC/HREC_config.h
@@ -0,0 +1,15 @@
+/************************************/
+/*	Auothor: MOHAMED ABOUBAKR       */
+/*	SWC:     ROTARY ENCODER         */
+/*	Layer:   HAL                    */
+/*	Version: 1.0                    */
+/*	Date:    OCTOBER 20, 2023       */
+/************************************/
+
+#ifndef _HREC_CONFIG_H_
+#define _HREC_CONFIG_H_
+
+#define  HREC_OUTPUT_A  MGPIO_PORTA,PIN0
+#define  HREC_OUTPUT_B  MGPIO_PORTA,PIN1
+
+#endif
