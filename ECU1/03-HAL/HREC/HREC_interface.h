a6da6eb9320d3e01747198c061eac51a7bedf697
Mohamed AbouBakr <itsmoaboubakr@outlook.com>
2023-10-20 20:55:28 +0300

    Update HREC_interface.h

--- ECU1/03-HAL/HREC/HREC_interface.h
+++ ECU1/03-HAL/HREC/HREC_interface.h
@@ -9,10 +9,11 @@
 #ifndef _HREC_INTERFACE_H_
 #define _HREC_INTERFACE_H_
 
-#define HREC_WHEEL_TURN       4
 #define HREC_PULSE_PER_REV    600
 
 void  HREC_voidInit(void);
+void  HREC_voidEnableEncoder(void);
+void  HREC_voidDisableEncoder(void);
 void  HREC_u16currentPosition(u16 *ARG_u16Angle, u8 *ARG_u8Rev);
 
 #endif
