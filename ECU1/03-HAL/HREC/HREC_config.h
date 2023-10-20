d2537addc5dc0a231ab10a6b6c537e95252a17dd
Mohamed AbouBakr <itsmoaboubakr@outlook.com>
2023-10-20 16:25:48 +0300

    Update HREC_config.h

--- ECU1/03-HAL/HREC/HREC_config.h
+++ ECU1/03-HAL/HREC/HREC_config.h
@@ -1,10 +1,10 @@
-/************************************/
-/*  Auothor: MOHAMED ABOUBAKR       */
-/*  SWC:     ROTARY ENCODER         */
-/*  Layer:   HAL                    */
-/*  Version: 1.0                    */
-/*  Date:    OCTOBER 20, 2023       */
-/************************************/
+/*************************************/
+/*  Auothor : MOHAMED ABOUBAKR       */
+/*  SWC     : ROTARY ENCODER         */
+/*  Layer   : HAL                    */
+/*  Version : 1.0                    */
+/*  Date    : OCTOBER 20, 2023       */
+/*************************************/
 
 #ifndef _HREC_CONFIG_H_
 #define _HREC_CONFIG_H_
