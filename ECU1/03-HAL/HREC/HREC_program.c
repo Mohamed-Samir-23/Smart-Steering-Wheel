0acba5a9681a85dd52081d98e5d82e0f853af4ed
Mohamed AbouBakr <itsmoaboubakr@outlook.com>
2023-11-03 22:24:40 +0200

    Update HREC_program.c

--- ECU1/03-HAL/HREC/HREC_program.c
+++ ECU1/03-HAL/HREC/HREC_program.c
@@ -11,117 +11,91 @@
 #include "LBIT_math.h"
 
 /* MCAL Includes */
-#include "MGPIO_interface.h"
+#include "MRCC_interface.h"
 #include "MTIM_private.h"
-//#include "MNVIC_interface.h"
 
 /* SWC Includes */
 #include "HREC_interface.h"
-#include "HREC_private.h"
 #include "HREC_config.h"
 
 
-u16 midPoint = HREC_WHEEL_TURN * HREC_PULSE_PER_REV;
-//volatile u16 encoderCount = 0;
-//u8 encoderDirection;
+volatile u16 L_u16encoderCount = 0;
 
 
 void HREC_voidInit(void)
 {
-	/* Enable RCC for GPIOA and TIM3 */
+	MRCC_stderrorInit(PLL_HSE, AHB_PreScaler1,APB_PreScaler2,APB_PreScaler1);
+
+	MRCC_stderrorSetPllClockFreq(PLL_PreScaler9, Wait_States_2);
+
+	/* Enable RCC for GPIOA and TIM4 */
 	RCC_EnablePeripheralClk( APB2, GPIOA_PER );
 	RCC_EnablePeripheralClk( APB2, AFIOEN_PER );
-	RCC_EnablePeripheralClk( APB1, TIM3EN_PER );
+	RCC_EnablePeripheralClk( APB1, TIM4EN_PER );
 	
-	/* Change PIN 0 and 1 in port A to alt func, push pull at 50mhz */
-	MGPIO_stderrorPinModeSelect(HREC_OUTPUT_A, ALTFUN_PUSH_PULL_SPEED_50MHZ );
-	MGPIO_stderrorPinModeSelect(HREC_OUTPUT_B, ALTFUN_PUSH_PULL_SPEED_50MHZ );
-
-	MGPIO_stderrorSetPinPull_Up_Down(HREC_OUTPUT_A, PULL_UP);
-	MGPIO_stderrorSetPinPull_Up_Down(HREC_OUTPUT_B, PULL_UP);
-
-	/*Partial remap: PB0 (TIM3_CH3) and PB1 (TIM3_CH4)*/
-	CLEAR_BIT(AFIO_MAPR, 10);
-	SET_BIT(AFIO_MAPR, 11);
 	
 	/* CC1S config as Capture TI1 */
-	SET_BIT(TIM3 -> CCMR1, 0);
-	CLEAR_BIT(TIM3 -> CCMR1, 1);
+	SET_BIT(TIM4 -> CCMR1, 0);
+	CLEAR_BIT(TIM4 -> CCMR1, 1);
 	
 	/* CC2S config as Capture TI2 */
-	SET_BIT(TIM3 -> CCMR1, 8);
-	CLEAR_BIT(TIM3 -> CCMR1, 9);
+	SET_BIT(TIM4 -> CCMR1, 8);
+	CLEAR_BIT(TIM4 -> CCMR1, 9);
 	
 	/* TI1FP1 noninverted,TI1FP1=TI1 */
-	CLEAR_BIT(TIM3 -> CCER, 1); // capture rising edge TI1
-	TIM3 -> CCMR1 &=  (~0b1111) << 4; //No filter, sampling is done for TI1
+	CLEAR_BIT(TIM4 -> CCER, 1);
 	
 	
 	/* TI2FP2 noninverted,TI2FP2=TI2 */
-	CLEAR_BIT(TIM3 -> CCER, 5); // capture rising edge TI2
-	TIM3 -> CCMR1 &=  (~0b1111) << 12; //No filter, sampling is done for TI2
+	CLEAR_BIT(TIM4 -> CCER, 5);
    
-   
-	/* Configure TIM3 in encoder mode */
-	TIM3 -> SMCR &= ~0b111; // Clear SMS bits for encoder mode
-	TIM3 -> SMCR |= 0b011; // Set SMS bits to count on TI1 and TI2 edges
+	/* Configure TIM4 in encoder mode */
+	TIM4 -> SMCR &= ~0b111; // Clear SMS bits for encoder mode
+	TIM4 -> SMCR |= 0b011; // Set SMS bits to count on TI1 and TI2 edges
 	
-	TIM3 -> CNT = midPoint; //set Preload to midpoint value
 	
-	TIM3 -> ARR = 0xFFFF; //set autoreload to max value
-	
-	SET_BIT(TIM3->CCER, 0 ); // Enable capture (input)
+	TIM4 -> ARR = 600; //set autoreload to max value
 
-	/*
-	SET_BIT( TIM3->DIER, 1 ); // Enable interrupt for input capture
-    
-	MNVIC_vEnableIRQ( TIM3_IRQn ); // Enable TIM3 interrupt in NVIC
-	*/
+	TIM4 -> PSC = 0x0000; //prescalar value
+	
 }
 
 void HREC_voidEnableEncoder(void)
 {
-	SET_BIT(TIM3->CR1, 0);
+	SET_BIT(TIM4->CR1, 0);
 }
 
 void HREC_voidDisableEncoder(void)
 {
-	CLEAR_BIT(TIM3->CR1, 0);
+	CLEAR_BIT(TIM4->CR1, 0);
 }
 
-void HREC_u16currentPosition( u8 *ARG_u8Rev, direction *ARG_directionState, u16 *ARG_u16Angle )
+void HREC_voidCurrentPosition(  u16 *ARG_u16Angle , direction *ARG_directionState, polarity *ARG_polarityCond)
 {
 	u16 L_u16absCount;
-	u16 L_u16EncoderCount = TIM3 -> CCR1;
+	L_u16encoderCount = TIM4 -> CNT;
 
-	if(L_u16EncoderCount > midPoint)
+	if(L_u16encoderCount > 0)
 	{
-		*ARG_u8Rev = ( L_u16encoderCount / HREC_PULSE_PER_REV ) - HREC_WHEEL_TURN;
-		*ARG_directionState = CLOCKWISE;
-		L_u16absCount = L_u16encoderCount - midPoint;
+		*ARG_polarityCond = POSITIVE;
+		L_u16absCount = L_u16encoderCount ;
 	}
-	else if(L_u16EncoderCount < midPoint)
+	else if(L_u16encoderCount < 0)
 	{
-		*ARG_u8Rev = HREC_WHEEL_TURN - ( L_u16encoderCount / HREC_PULSE_PER_REV );
-		*ARG_directionState = ANTICLOCKWISE;
-		L_u16absCount = midPoint - L_u16encoderCount;
+		//*ARG_u8Rev = HREC_WHEEL_TURN - ( L_u16encoderCount / HREC_PULSE_PER_REV );
+		*ARG_polarityCond = NEGATIVE;
+		L_u16absCount = HREC_PULSE_PER_REV - L_u16encoderCount;
 	}
 	else{/* Do Nothing */}
 
+	if( GET_BIT(TIM3 -> CR1, 4 ) )
+	{
+		*ARG_directionState = ANTICLOCKWISE;
+	}
+	else
+	{
+		*ARG_directionState = CLOCKWISE;
+	}
+
 	*ARG_u16Angle = ( L_u16absCount * 360 ) / HREC_PULSE_PER_REV;
 }
-
-/*
-void TIM3_IRQHandler()
-{
-	if ( GET_BIT (TIM3 -> SR, 1 ) )
-	{
-		// Input capture event occurred on channel 1
-		// Read the encoder count from the corresponding register
-		encoderCount = TIM3 -> CCR1;
-		
-		//Check if DIR bit is 0 upcounter or 1 downcounter
-		encoderDirection = GET_BIT(TIM3 -> CR1, 4 );
-	}
-}
-*/
